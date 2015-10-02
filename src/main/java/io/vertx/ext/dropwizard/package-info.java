2b3574d5b2309bf54386f934e9c8526a2fc44cfa
Clement Escoffier <clement.escoffier@gmail.com>
2015-10-02 19:08:58 +0200

    Replace asciidoc attributes by docgen variables

--- src/main/java/io/vertx/ext/dropwizard/package-info.java
+++ src/main/java/io/vertx/ext/dropwizard/package-info.java
@@ -21,9 +21,9 @@
  * [source,xml,subs="+attributes"]
  * ----
  * <dependency>
- *   <groupId>{maven-groupId}</groupId>
- *   <artifactId>{maven-artifactId}</artifactId>
- *   <version>{maven-version}</version>
+ *   <groupId>${maven.groupId}</groupId>
+ *   <artifactId>${maven.artifactId}</artifactId>
+ *   <version>${maven.version}</version>
  * </dependency>
  * ----
  *
@@ -31,7 +31,7 @@
  *
  * [source,groovy,subs="+attributes"]
  * ----
- * compile {maven-groupId}:{maven-artifactId}:{maven-version}
+ * compile ${maven.groupId}:${maven.artifactId}:${maven.version}
  * ----
  *
  * Then when you create vertx enable metrics using the {@link io.vertx.ext.dropwizard.DropwizardMetricsOptions}:
