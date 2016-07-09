45c96a4b63328be8964d5712b58af0cf3b248f87
Julien Viet <julien@julienviet.com>
2016-07-09 18:19:33 +0200

    Revert "Use a regex for testing rather"
    This reverts commit 27a4345d096fd8e0a194470de8ba390ab0ffa87a.

--- src/test/java/io/vertx/ext/dropwizard/MetricsTest.java
+++ src/test/java/io/vertx/ext/dropwizard/MetricsTest.java
@@ -101,7 +101,7 @@ public class MetricsTest extends MetricsTestBase {
                 addMonitoredEventBusHandler(new Match().setValue("juu.*").setType(MatchType.REGEX)).
                 addMonitoredHttpServerUri(new Match().setValue("/get")).
                 addMonitoredHttpServerUri(new Match().setValue("/p.*").setType(MatchType.REGEX)).
-                addMonitoredHttpClientEndpoint(new Match().setValue("localhost:.*").setType(MatchType.REGEX))
+                addMonitoredHttpClientEndpoint(new Match().setValue("localhost:8080"))
         );
   }
 
