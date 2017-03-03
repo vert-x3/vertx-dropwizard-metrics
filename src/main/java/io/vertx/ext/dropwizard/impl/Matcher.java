d20a246c2db7517be3f20f7472d1f6fb478dc919
jhony_chikens <evts@gmail.com>
2017-03-03 21:43:21 +0100

    changes requested by code review - use array of SimpleEntry instead of HashMap in order to avoid creation of an iterator

--- src/main/java/io/vertx/ext/dropwizard/impl/Matcher.java
+++ src/main/java/io/vertx/ext/dropwizard/impl/Matcher.java
@@ -4,6 +4,7 @@ import io.vertx.ext.dropwizard.Match;
 import io.vertx.ext.dropwizard.MatchType;
 
 import java.util.*;
+import java.util.AbstractMap.SimpleEntry;
 import java.util.Map.Entry;
 import java.util.regex.Pattern;
 
@@ -12,25 +13,27 @@ import java.util.regex.Pattern;
  */
 class Matcher {
   private final Set<String> equalsMatches;
-  private final Map<Pattern, String> regexMatches;
+  private final Entry<Pattern, String>[] regexMatches;
   private final Map<String, String> identifiers;
 
   Matcher(List<Match> matches) {
     equalsMatches = new HashSet<>();
-    regexMatches = new HashMap<>();
     identifiers = new HashMap<>();
 
     for (Match match : matches) {
       if (match.getType() == MatchType.EQUALS && match.getValue() != null) {
         equalsMatches.add(match.getValue());
-      } else if (match.getType() == MatchType.REGEX && match.getValue() != null) {
-        regexMatches.put(Pattern.compile(match.getValue()), match.getValue());
       }
 
       if (match.getIdentifier() != null) {
         identifiers.put(match.getValue(), match.getIdentifier());
       }
     }
+
+    regexMatches = matches.stream()
+      .filter(match -> match.getType() == MatchType.REGEX && match.getValue() != null)
+      .map(match -> new SimpleEntry<>(Pattern.compile(match.getValue()), match.getValue()))
+      .toArray(Entry[]::new);
   }
 
   String match(String value) {
@@ -38,8 +41,8 @@ class Matcher {
       return value;
     }
 
-    if (regexMatches.size() > 0) {
-      for (Entry<Pattern, String> entry : regexMatches.entrySet()) {
+    if (regexMatches.length > 0) {
+      for (Entry<Pattern, String> entry : regexMatches) {
         if (entry.getKey().matcher(value).matches()) {
           return entry.getValue();
         }
