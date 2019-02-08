16bc90dbd60dd1552e155ce56f4158e3bd4249b8
Julien Viet <julien@julienviet.com>
2019-02-08 17:36:13 +0100

    VertxMetricsImpl.createHttpClientMetrics should use a ConcurrentMap - fixes #89

--- src/main/java/io/vertx/ext/dropwizard/impl/VertxMetricsImpl.java
+++ src/main/java/io/vertx/ext/dropwizard/impl/VertxMetricsImpl.java
@@ -32,9 +32,9 @@ import io.vertx.core.spi.metrics.*;
 import io.vertx.ext.dropwizard.DropwizardMetricsOptions;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * @author <a href="mailto:nscavell@redhat.com">Nick Scavelli</a>
@@ -45,7 +45,7 @@ class VertxMetricsImpl extends AbstractMetrics implements VertxMetrics {
   private final Counter verticles;
   private Handler<Void> doneHandler;
   private final boolean shutdown;
-  private final Map<String, HttpClientReporter> clientReporters = new HashMap<>();
+  private final Map<String, HttpClientReporter> clientReporters = new ConcurrentHashMap<>();
 
   VertxMetricsImpl(MetricRegistry registry, boolean shutdown, VertxOptions options, DropwizardMetricsOptions metricsOptions, String baseName) {
     super(registry, baseName);
