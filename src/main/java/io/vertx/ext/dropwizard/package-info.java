f97c24cdc1f71b5dd1bd80181055df069fa876b7
Thomas Segismont <tsegismont@gmail.com>
2023-03-03 16:10:41 +0100

    See vert-x3/issues#612
    Easy one, no callbacks in this module
    
    Signed-off-by: Thomas Segismont <tsegismont@gmail.com>

--- src/main/java/io/vertx/ext/dropwizard/package-info.java
+++ src/main/java/io/vertx/ext/dropwizard/package-info.java
@@ -1,4 +1,4 @@
-@ModuleGen(name = "vertx-dropwizard", groupPackage = "io.vertx")
+@ModuleGen(name = "vertx-dropwizard", groupPackage = "io.vertx", checkCallbackDeprecation = true)
 package io.vertx.ext.dropwizard;
 
 import io.vertx.codegen.annotations.ModuleGen;
