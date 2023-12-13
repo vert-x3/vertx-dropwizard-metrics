3a567ee7a8216f246f668e301a8d9b09b8268002
Julien Viet <julien@julienviet.com>
2023-12-13 16:58:58 +0100

    Set version to 4.5.2-SNAPSHOT

--- src/main/generated/io/vertx/ext/dropwizard/DropwizardMetricsOptionsConverter.java
+++ src/main/generated/io/vertx/ext/dropwizard/DropwizardMetricsOptionsConverter.java
@@ -17,7 +17,7 @@ public class DropwizardMetricsOptionsConverter {
   private static final Base64.Decoder BASE64_DECODER = JsonUtil.BASE64_DECODER;
   private static final Base64.Encoder BASE64_ENCODER = JsonUtil.BASE64_ENCODER;
 
-  public static void fromJson(Iterable<java.util.Map.Entry<String, Object>> json, DropwizardMetricsOptions obj) {
+   static void fromJson(Iterable<java.util.Map.Entry<String, Object>> json, DropwizardMetricsOptions obj) {
     for (java.util.Map.Entry<String, Object> member : json) {
       switch (member.getKey()) {
         case "baseName":
@@ -98,11 +98,11 @@ public class DropwizardMetricsOptionsConverter {
     }
   }
 
-  public static void toJson(DropwizardMetricsOptions obj, JsonObject json) {
+   static void toJson(DropwizardMetricsOptions obj, JsonObject json) {
     toJson(obj, json.getMap());
   }
 
-  public static void toJson(DropwizardMetricsOptions obj, java.util.Map<String, Object> json) {
+   static void toJson(DropwizardMetricsOptions obj, java.util.Map<String, Object> json) {
     if (obj.getBaseName() != null) {
       json.put("baseName", obj.getBaseName());
     }
