5598872b55547db6d036e8f0b982193995ab0767
Julien Viet <julien@julienviet.com>
2024-09-23 10:08:24 +0200

    JPMS support

---
+++ src/test/java/io/vertx/ext/dropwizard/it/VertxServiceProviderTest.java
@@ -0,0 +1,23 @@
+package io.vertx.ext.dropwizard.it;
+
+import io.vertx.core.Vertx;
+import io.vertx.core.VertxOptions;
+import io.vertx.core.internal.VertxInternal;
+import io.vertx.core.metrics.MetricsOptions;
+import io.vertx.core.spi.metrics.VertxMetrics;
+import io.vertx.ext.dropwizard.impl.VertxMetricsImpl;
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+public class VertxServiceProviderTest {
+
+  @Test
+  public void testEnabled() {
+    Vertx vertx = Vertx.vertx(new VertxOptions().setMetricsOptions(new MetricsOptions().setEnabled(true)));
+    VertxMetrics spi = ((VertxInternal) vertx).metricsSPI();
+    assertNotNull(spi);
+    assertTrue(spi instanceof VertxMetricsImpl);
+  }
+}
