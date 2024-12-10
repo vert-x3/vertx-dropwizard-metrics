b8b48e115f22081a1c8614aa51b695d1acfe95d3
Julien Viet <julien@julienviet.com>
2024-12-10 16:41:14 +0100

    Remove usage of useFutures member

--- src/main/java/io/vertx/ext/dropwizard/package-info.java
+++ src/main/java/io/vertx/ext/dropwizard/package-info.java
@@ -1,4 +1,4 @@
-@ModuleGen(name = "vertx-dropwizard", groupPackage = "io.vertx", useFutures = true)
+@ModuleGen(name = "vertx-dropwizard", groupPackage = "io.vertx")
 package io.vertx.ext.dropwizard;
 
 import io.vertx.codegen.annotations.ModuleGen;
