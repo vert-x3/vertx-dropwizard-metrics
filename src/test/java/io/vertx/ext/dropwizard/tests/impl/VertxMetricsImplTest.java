068a2a644f49a2c64a0bcf453361db24ab415bd2
Thomas Segismont <tsegismont@gmail.com>
2025-09-15 17:43:47 +0200

    Default pattern for HTTP monitored endpoints should be a regexp
    Fixes #124
    
    A wildcard pattern should be flagged as regular expression.
    
    Signed-off-by: Thomas Segismont <tsegismont@gmail.com>

--- src/test/java/io/vertx/ext/dropwizard/tests/impl/VertxMetricsImplTest.java
+++ src/test/java/io/vertx/ext/dropwizard/tests/impl/VertxMetricsImplTest.java
@@ -1,7 +1,5 @@
 package io.vertx.ext.dropwizard.tests.impl;
 
-import static org.junit.Assert.assertNotNull;
-
 import com.codahale.metrics.MetricRegistry;
 import io.vertx.core.VertxOptions;
 import io.vertx.core.spi.metrics.PoolMetrics;
@@ -9,12 +7,14 @@ import io.vertx.ext.dropwizard.DropwizardMetricsOptions;
 import io.vertx.ext.dropwizard.impl.VertxMetricsImpl;
 import org.junit.Test;
 
+import static org.junit.Assert.assertNotNull;
+
 /**
  * @author Siarhei.Bahdanchuk Date: 29.07.2025
  */
 public class VertxMetricsImplTest {
 
-  @Test(expected = Test.None.class)
+  @Test
   public void testCreatePoolMetricsShouldNotThrowNPE() {
     VertxMetricsImpl vmi = new VertxMetricsImpl(new MetricRegistry(), false,
       new VertxOptions(), new DropwizardMetricsOptions(), "baseName");
